c67436bf0d2de70809d624a2aba8708bbc0af414
Eslam ElTayyar <islamdiaa@fb.com>
2020-07-05 02:13:34 +0100

    Pagination frontend

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import Block from './Block';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 class Blocks extends Component {
-  state = { blocks: [] };
+  state = { blocks: [], paginatedId: 1, blocksLength: 0 };
 
   componentDidMount() {
-    fetch(`${document.location.origin}/api/blocks`).then(
-      response => response.json()).then(
-        json => {
-          this.setState({blocks: json});
+    fetch(`${document.location.origin}/api/blocks/length`)
+      .then(response => response.json())
+      .then(json => this.setState({ blocksLength: json }));
+
+    this.fetchPaginatedBlocks(this.state.paginatedId)();
+  }
+
+  fetchPaginatedBlocks = paginatedId => () => {
+    fetch(`${document.location.origin}/api/blocks/${paginatedId}`)
+      .then(response => response.json())
+      .then(json => this.setState({ blocks: json }));
+  }
+
+  getPaginationComponent() {
+    return (
+      <div>
+        {
+          [...Array(Math.ceil(this.state.blocksLength/5)).keys()].map(key => {
+            const paginatedId = key+1;
+
+            return (
+              <span
+                key={key}
+                onClick={() => this.fetchPaginatedBlocks(paginatedId)}>
+                <Button bsSize="small" bsStyle="danger">
+                  {paginatedId}
+                </Button>{' '}
+              </span>
+            )
+          })
         }
-      );
+      </div>
+    );
   }
 
   render() {
@@ -23,6 +51,7 @@ class Blocks extends Component {
         <div className='BlocksContainer'>
           <div><Link to='/'>Home</Link></div>
           <div><h3>Blocks</h3></div>
+          {this.getPaginationComponent()}
           {blocksComponents}
         </div>
       </div>
